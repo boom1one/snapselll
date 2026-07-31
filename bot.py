6d33d7cf25a962fbab379940da162ebdc308320e
boom1one <rotrol1232@gmail.com>
2026-07-31 21:34:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from flask import Flask, request, jsonify
 from datetime import datetime, timedelta
 
 # ========== КОНФИГУРАЦИЯ ==========
-TOKEN = "8977186531:AAFwl7w9GWT7zDPBWHmTF4KQzD6npHQ8i5U"
+TOKEN = "8857819530:AAF_XClRgpje6cZ08HDZMEVGyXqMnVUyqNE"
 CHANNEL_ID = "@SnapSell350"
 BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
 
